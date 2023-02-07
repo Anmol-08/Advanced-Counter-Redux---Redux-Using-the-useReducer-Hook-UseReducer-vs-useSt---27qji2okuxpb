135402c1f9818e12ccfc870e3f80186aee1c8e05
Anmol <85445670+Anmol-08@users.noreply.github.com>
2023-02-07 12:57:24 +0530

    Update action.js

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,20 +1,30 @@
-export const decrement = () => ({
-  type: 'DECREMENT'
-});
+export const addNumber=()=>{
+    return{
+        type: 'INCREMENT'
+    }
+}
 
-export const increment = () => ({
-  type: 'INCREMENT'
-});
+export const subNumber=()=>{
+    return{
+        type: 'DECREMENT'
+    }
+}
 
-export const incrementByAmount = amount => ({
-  type: 'INCREMENTBYAMOUNT',
-  payload: amount
-});
+export const handlelogin=()=>{
+    return{
+        type: 'SIGN_IN'
+    }
+}
 
-export const signIn = () => ({
-  type: 'SIGN_IN'
-});
+export const handlelogout=()=>{
+    return{
+        type: 'SIGN_OUT'
+    }
+}
 
-export const signOut = () => ({
-  type: 'SIGN_OUT'
-});
+export const addAmount=(val)=>{
+    return{
+        type: 'INCREMENTBYAMOUNT',
+        payload: val
+    }
+}
