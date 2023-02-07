4d7a52c8a0532f1dc44619bc5fe61f1e49cc9ccf
Anmol <85445670+Anmol-08@users.noreply.github.com>
2023-02-07 12:49:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { decrement, increment, incrementByAmount, signIn, signOut } from './actions';
 
-function App() {
+const App = ({ counter, isLogged, increment, decrement, incrementByAmount, signIn, signOut }) => {
+  const [amount, setAmount] = useState(0);
+
+  const handleAmountChange = e => {
+    setAmount(Number(e.target.value));
+  };
 
   return (
     <div id='main'>
-      
+      {isLogged ? (
+        <div>
+          <div data-testid='counter'>{counter}</div>
+          <button onClick={decrement}>-</button>
+          <button onClick={increment}>+</button>
+          <input type='number' value={amount} onChange={handleAmountChange} />
+          <button onClick={() => incrementByAmount(amount)}>Add Amount</button>
+          <button onClick={signOut}>Logout</button>
+        </div>
+      ) : (
+        <button onClick={signIn}>Login</button>
+      )}
     </div>
   );
-}
+};
 
-export default App;
+const mapStateToProps = state => ({
+  counter: state.counter,
+  isLogged: state.isLogged
+});
+
+const mapDispatchToProps = {
+  decrement,
+  increment,
+  incrementByAmount,
+  signIn,
+  signOut
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
